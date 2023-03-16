6284ab6fb76547404baa854a16ec60ea329e3c43
navyanutakki <72597138+navyanutakki@users.noreply.github.com>
2023-03-16 12:03:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -31,6 +31,9 @@ JOBS=[
 @app.route('/')
 def hello_jovian():
     return render_template('home.html',jobs=JOBS)
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__=='__main__':
   app.run(host='0.0.0.0', debug=True)
